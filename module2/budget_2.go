bf3d2e7ba6794f9a7c9c28b0451dc0de35f3fa8c
Craig Cartlidge <craig.cartlidge@whitespider.com>
2022-01-06 08:42:11 +0000

    try again

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -65,7 +65,7 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-			b.Items = (b.Items[:i],b.Items...[i+1]...)
+			b.Items = append(b.Items[:i], b.Items...[i+1:]...)
 			break
 
 		}
